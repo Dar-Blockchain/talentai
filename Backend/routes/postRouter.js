a88927d8dd05cbe89b49e7da676a6eef5ba5163c
AzizBenIsmail <aziz.270700@gmail.com>
2025-04-22 16:03:17 +0100

    fix router post

--- Backend/routes/postRouter.js
+++ Backend/routes/postRouter.js
@@ -1,41 +1,33 @@
 const express = require('express');
 const router = express.Router();
-const { authenticateToken } = require('../middleware/auth');
-const {
-  createPost,
-  getAllPosts,
-  getPostById,
-  getUserPosts,
-  updatePost,
-  deletePost,
-  updatePostStatus
-} = require('../controllers/postController');
+const { authMiddleware } = require('../middleware/authMiddleware');
+const postController = require('../controllers/postController');
 
 // Routes protégées par authentification
-router.use(authenticateToken);
+router.use(authMiddleware);
 
 // Route pour créer un post
-router.post('/', createPost);
+router.post('/', postController.createPost);
 
 // Route pour récupérer tous les posts
-router.get('/', getAllPosts);
+router.get('/', postController.getAllPosts);
 
 // Route pour récupérer les posts de l'utilisateur connecté
-router.get('/my-posts', getUserPosts);
+router.get('/my-posts', postController.getUserPosts);
 
 // Route pour récupérer les posts d'un utilisateur spécifique
-router.get('/user/:userId', getUserPosts);
+router.get('/user/:userId', postController.getUserPosts);
 
 // Route pour récupérer un post spécifique
-router.get('/:id', getPostById);
+router.get('/:id', postController.getPostById);
 
 // Route pour mettre à jour un post
-router.put('/:id', updatePost);
+router.put('/:id', postController.updatePost);
 
 // Route pour changer le statut d'un post
-router.patch('/:id/status', updatePostStatus);
+router.patch('/:id/status', postController.updatePostStatus);
 
 // Route pour supprimer un post
-router.delete('/:id', deletePost);
+router.delete('/:id', postController.deletePost);
 
 module.exports = router; 
