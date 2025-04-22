2db187ad0257c424bf94abeb463c40fbf6ad5f86
AzizBenIsmail <aziz.270700@gmail.com>
2025-04-22 09:56:49 +0100

    Update linkedinPostRouter.js

--- Backend/routes/linkedinPostRouter.js
+++ Backend/routes/linkedinPostRouter.js
@@ -7,11 +7,6 @@ const { requireAuthUser } = require('../middleware/authMiddleware');
 // Routes protégées par authentification
 router.use(requireAuthUser);
 
-//router.get('/generate-questions', (req, res) => {
-//  res.json({ message: 'Endpoint is active. Please use POST method with required skills and experience.' });
-//});
-
-
 router.post('/generate-job-post', linkedinPostController.generateJobPost);
 
 module.exports = router; 
