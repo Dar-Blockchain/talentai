7868b30dc0268174ef439df61664d251651336cc
monabeldi <beldi.mona@esprit.tn>
2025-05-30 11:30:03 +0200

    update todoList and its management

--- Backend/controllers/todoController.js
+++ Backend/controllers/todoController.js
@@ -1,10 +1,10 @@
-const { Together } = require("together-ai");
 require("dotenv").config();
 
-const ToDo = require("../models/TodoModel");
+// const ToDo = require("../models/TodoModel");
+const TodoList = require("../models/todoListModel");
 const Profile = require("../models/ProfileModel");
 
-const together = new Together({ apiKey: process.env.TOGETHER_API_KEY });
+const { generateNewTodosForProfile } = require("../services/todoService");
 
 exports.generateTodoListForProfile = async (req, res) => {
   try {
@@ -26,114 +26,18 @@ exports.generateTodoListForProfile = async (req, res) => {
         .json({ error: "No skills found in the user profile." });
     }
 
-    // Format skills into prompt
-    const formattedSkills = profile.skills
-      .map(
-        (s) =>
-          `${s.name} (Experience: ${s.experienceLevel}, Proficiency: ${s.proficiencyLevel}/5)`
-      )
-      .join(", ");
+    let todo = await TodoList.findOne({ profile: profile._id });
 
-    let todo = await ToDo.findOne({ profile: profile._id });
-
-    let existingSkillTodoList = [];
-    if (todo) {
-      todo.todos.forEach((t) => {
-        if (t.type == "Skill") {
-          existingSkillTodoList.push(t);
-        }
-      });
-    }
-
-    // Prompt for generating skill-based recommendations
-    const prompt = `
-You are a career development AI assistant.
-Given:
-- A candidate with the following skills:
-${formattedSkills}
-- A condidate that has a list of todos for each skill: 
-${existingSkillTodoList}
-
-
-Generate a JSON array where each item is an object with:
-- title: the name of the skill
-- type:"Skill"
-- tasks (if type is "Skill"): array of up to 3 objects, each with:
-  - title: what to do (e.g., "Take XYZ course", "Get ABC certification")
-  - type: STRICTLY one of **"Course", "Certification","Project","Article","Video", or "Book"** (No other values allowed).
-  - description: a brief description of the task
-  - url (optional)
-  - priority: STRICTLY one of **"Low", "Medium", or "High"** (No other values allowed).
-  - dueDate: A recommended completion deadline in timestamp format.
-
-STRICT REQUIREMENTS:
-- for each skill:  title, type, description, priority are required fields
-- For each skill, **DO NOT include tasks similar in purpose to existing ones, that are already accorded for that task** Ensure each recommendation **adds new skill-improvement value**.
-- Offer a unique learning perspective.
-
-Return a STRICT JSON array in this format ONLY (no markdown, no explanation):
-
-[
-  {
-    "title": "JavaScript",
-    "tasks": [
-      {
-        "title": "Take the JavaScript Algorithms course on freeCodeCamp",
-        "type": "Course",
-        "description": "Covers fundamental JS algorithms with interactive coding exercises",
-        "url": "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures", 
-        "priority": "medium",
-        "dueDate": 1748531525930
-      },
-      {
-        "title": "Build a ToDo App using vanilla JavaScript",
-        "type": "Project",
-        "description": "Apply JS concepts to create a CRUD-based web application",
-        "priority": "medium",
-        "dueDate": 1748531525930
-      }
-    ]
-  },
-  ...
-]
-`.trim();
-
-    const stream = await together.chat.completions.create({
-      model: "meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo",
-      messages: [{ role: "system", content: prompt }],
-      max_tokens: 1000,
-      temperature: 0.7,
-      stream: true,
-    });
-
-    let raw = "";
-    for await (const chunk of stream) {
-      const content = chunk.choices?.[0]?.delta?.content;
-      if (content) raw += content;
-    }
-
-    // Parse JSON output
-    const jsonMatch = raw.match(/\[([\s\S]*)\]/);
-    let newTodos = [];
-
-    if (jsonMatch) {
-      const jsonText = "[" + jsonMatch[1] + "]";
-      try {
-        newTodos = JSON.parse(jsonText);
-      } catch (err) {
-        console.warn("Parsing failed:", err);
-        return res.status(500).json({ error: "Failed to parse AI output." });
-      }
-    }
-    // Upsert ToDo
-    const defaultProfileTodos = [
-      { type: "Profile", title: "Complete Your Profile", isCompleted: false },
-      { type: "Profile", title: "Upload CV", isCompleted: false },
-    ];
+    let newTodos = await generateNewTodosForProfile(todo, profile);
 
     let skillTodos = newTodos;
     if (!todo) {
-      todo = await ToDo.create({
+      // Upsert ToDo
+      const defaultProfileTodos = [
+        { type: "Profile", title: "Upload CV", isCompleted: false },
+        { type: "Skill", title: "Add Skill", isCompleted: false },
+      ];
+      todo = await TodoList.create({
         profile: profile._id,
         todos: [...defaultProfileTodos, ...newTodos],
       });
@@ -190,14 +94,51 @@ exports.getTodoListOfProfile = async (req, res) => {
         error: `profile of the user with userId ${user._id} not found in the db `,
       });
     }
-    const todoList = await ToDo.findById(profile.todoList);
-    if (!todoList) {
-      return res.status(200).json({
-        error: `profile of the user with userId ${user._id} does not have a todoList yet`,
-      });
+    let todo = await TodoList.findById(profile.todoList);
+    if (!todo) {
+      let newTodos = await generateNewTodosForProfile(todo, profile);
+      let skillTodos = newTodos;
+      if (!todo) {
+        // Upsert ToDo
+        const defaultProfileTodos = [
+          { type: "Profile", title: "Upload CV", isCompleted: false },
+          { type: "Skill", title: "Add Skill", isCompleted: false },
+        ];
+        todo = await TodoList.create({
+          profile: profile._id,
+          todos: [...defaultProfileTodos, ...newTodos],
+        });
+        profile.todoList = todo._id;
+        await profile.save();
+      } else {
+        skillTodos.forEach((skillTodo) => {
+          const existingSkillTodo = todo.todos.find(
+            (t) => t.title === skillTodo.title && t.type === "Skill"
+          );
+
+          // Add only up to 5 total tasks
+          if (existingSkillTodo) {
+            const remainingTodos = Math.max(
+              0,
+              5 - existingSkillTodo.tasks.length
+            );
+
+            if (remainingTodos > 0) {
+              existingSkillTodo.tasks.push(
+                ...skillTodo.tasks.slice(0, remainingTodos)
+              );
+            }
+          } else {
+            skillTodo.tasks = skillTodo.tasks.slice(0, 5); // Ensure new skill doesn't exceed the limit
+            todo.todos.push(skillTodo);
+          }
+        });
+
+        await todo.save();
+      }
     }
 
-    return res.status(200).json({ todos: todoList.todos });
+    return res.status(200).json({ todos: todo.todos });
   } catch (error) {
     console.error("Error generating ToDo list:", error);
     res.status(500).json({ error: "Failed to generate ToDo list" });
