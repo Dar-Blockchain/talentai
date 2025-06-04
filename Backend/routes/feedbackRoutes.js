87b6f574407320f4d4fbb003dae369192f726102
AzizBenIsmail <aziz.270700@gmail.com>
2025-06-04 16:19:12 +0100

    fix feedback

--- Backend/routes/feedbackRoutes.js
+++ Backend/routes/feedbackRoutes.js
@@ -6,9 +6,6 @@ const feedbackController = require('../controllers/feedbackController');
 const {requireAuthUser} = require('../middleware/authMiddleware');
 
 router.post('/addFeedback', requireAuthUser, feedbackController.create);
-router.get('/getAllFeedback', feedbackController.getAll);
-router.get('/:id', feedbackController.getOne);
-router.put('/:id', requireAuthUser, feedbackController.update);
-router.delete('/:id', requireAuthUser, feedbackController.remove);
+router.get('/getAllFeedback', feedbackController.getAllFeedback);
 
 module.exports = router;
--- Backend/services/feedbackService.js
+++ Backend/services/feedbackService.js
@@ -7,15 +7,3 @@ module.exports.createFeedback = async (data) => {
 module.exports.getAllFeedbacks = async () => {
   return await Feedback.find().populate('userId', 'name email');
 };
-
-module.exports.getFeedbackById = async (id) => {
-  return await Feedback.findById(id).populate('userId', 'name email');
-};
-
-module.exports.updateFeedback = async (id, data) => {
-  return await Feedback.findByIdAndUpdate(id, data, { new: true });
-};
-
-module.exports.deleteFeedback = async (id) => {
-  return await Feedback.findByIdAndDelete(id);
-};
