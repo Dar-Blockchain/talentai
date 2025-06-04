e7b139ac6348961bd49750e730c7db7ddb959f92
AzizBenIsmail <aziz.270700@gmail.com>
2025-06-04 16:10:36 +0100

    Update feedbackRoutes.js

--- Backend/routes/feedbackRoutes.js
+++ Backend/routes/feedbackRoutes.js
@@ -5,8 +5,8 @@ const feedbackController = require('../controllers/feedbackController');
 // Middleware d'authentification (optionnel)
 const {requireAuthUser} = require('../middleware/authMiddleware');
 
-router.post('/', requireAuthUser, feedbackController.create);
-router.get('/', feedbackController.getAll);
+router.post('/addFeedback', requireAuthUser, feedbackController.create);
+router.get('/getAllFeedback', feedbackController.getAll);
 router.get('/:id', feedbackController.getOne);
 router.put('/:id', requireAuthUser, feedbackController.update);
 router.delete('/:id', requireAuthUser, feedbackController.remove);
