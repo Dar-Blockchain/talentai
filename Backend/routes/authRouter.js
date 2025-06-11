b936fa85584a2f2dcd258f5411798e674c3924a0
AzizBenIsmail <aziz.270700@gmail.com>
2025-06-11 12:20:52 +0100

    Fix Sécurité connect With Gmail

--- Backend/routes/authRouter.js
+++ Backend/routes/authRouter.js
@@ -12,6 +12,9 @@ router.post("/verify-otp", authController.verifyOTP);
 // Route de connexion avec Gmail
 router.post("/connect-gmail", authController.connectWithGmail);
 
+router.get("/GetEmailGmailByToken", authController.GetEmailGmailByToken);
+
+
 router.get("/getAllUsers", requireAuthUser, authController.getAllUsers);
 
 // Route de déconnexion
