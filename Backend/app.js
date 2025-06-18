f2f228e6147fcd69de68eb6a356c409c6dd76f27
jassemtalbi <jassemtalbi2@gmail.com>
2025-06-18 12:39:53 +0100

    Update CORS configuration to restrict origins to specific domain

--- Backend/app.js
+++ Backend/app.js
@@ -33,8 +33,8 @@ app.use(blockPostmanRequests);
 app.use(express.json());
 app.use(
   cors({
-   // origin: "https://app.talentai.bid", // Permet toutes les origines
-     origin:  "*", // Permet toutes les origines
+    origin: "https://app.talentai.bid", // Permet toutes les origines
+    //  origin:  "*", // Permet toutes les origines
     methods: "GET, POST, PUT, DELETE, PATCH",
     allowedHeaders:
       "Origin, X-Requested-With, Content-Type, Accept, Authorization",
