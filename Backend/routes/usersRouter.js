9570e2a726e77fdeefccdf3d447cc93123bbfc70
AzizBenIsmail <aziz.270700@gmail.com>
2025-06-20 09:52:36 +0100

    getAllUsers with pagination

--- Backend/routes/usersRouter.js
+++ Backend/routes/usersRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const usersController = require("../controllers/usersController");
 const { requireAuthUser } = require("../middleware/authMiddleware");
 
-router.get("/getAllUsers", requireAuthUser, usersController.getAllUsers);
+router.get("/getAllUsers", usersController.getAllUsers);
 
 
 module.exports = router;
