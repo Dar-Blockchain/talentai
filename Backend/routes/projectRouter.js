138eb548788e9f81d1903a7e2272217299634762
AzizBenIsmail <aziz.270700@gmail.com>
2025-07-01 11:29:50 +0100

    fix bug corss

--- Backend/routes/projectRouter.js
+++ Backend/routes/projectRouter.js
@@ -4,10 +4,12 @@ const projectController = require("../controllers/projectContoller");
 const { requireAuthUser } = require('../middleware/authMiddleware');
 const authLogMiddleware = require("../middleware/SystemeLogs/LogMiddleware")
 
-router.post("/addProject",requireAuthUser, authLogMiddleware("Project"), projectController.createProject); // Créer un projet
-router.get("/getAllProjects",requireAuthUser, authLogMiddleware("Project"), projectController.getAllProjects); // Récupérer tous les projets
-router.get("/getProjectById/:id",requireAuthUser, authLogMiddleware("Project"), projectController.getProjectById); // Récupérer un projet par ID
-router.put("/updateProject/:id",requireAuthUser, authLogMiddleware("Project"), projectController.updateProject); // Mettre à jour un projet
-router.delete("/deleteProject/:id",requireAuthUser, authLogMiddleware("Project"), projectController.deleteProject); // Supprimer un projet
+//router.use(requireAuthUser, authLogMiddleware("Project"));
+
+router.post("/addProject", projectController.createProject); // Créer un projet
+router.get("/getAllProjects", projectController.getAllProjects); // Récupérer tous les projets
+router.get("/getProjectById/:id", projectController.getProjectById); // Récupérer un projet par ID
+router.put("/updateProject/:id", projectController.updateProject); // Mettre à jour un projet
+router.delete("/deleteProject/:id", projectController.deleteProject); // Supprimer un projet
 
 module.exports = router;
