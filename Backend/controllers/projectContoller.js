97792398ba04ec2b80fe6f234acdf05a5ee0b0e1
AzizBenIsmail <aziz.270700@gmail.com>
2025-07-01 11:34:21 +0100

    add requireAuthUser authLogMiddleware

--- Backend/controllers/projectContoller.js
+++ Backend/controllers/projectContoller.js
@@ -4,7 +4,7 @@ const projectService = require("../services/projectService");
 module.exports.createProject = async (req, res) => {
   try {
     const data = req.body;
-    data.leaderId = "6862af00e704e223cf63fccc"
+    data.leaderId = req.user._id
     const newProject = await projectService.createProject(data);
     res.status(201).json(newProject);
   } catch (error) {
