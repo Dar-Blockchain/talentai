aa7466e6bf8c8c744970a4b9ee349e00e03e9cc5
AzizBenIsmail <aziz.270700@gmail.com>
2025-07-01 16:41:23 +0100

    add activationToken

--- Backend/models/projectModel.js
+++ Backend/models/projectModel.js
@@ -16,6 +16,7 @@ const ProjectSchema = new mongoose.Schema(
           type: Boolean,
           default: false, // Le champ `validated` pour chaque membre
         },
+        activationToken: String, // Ajoute ce champ
       },
     ],
     leaderId: {
