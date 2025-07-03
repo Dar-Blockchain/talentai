7d04600f0160f74a2a90fd2606396ef23e8700c5
AzizBenIsmail <aziz.270700@gmail.com>
2025-07-03 11:40:01 +0100

    Update evaluationRouter.js

--- Backend/routes/evaluationRouter.js
+++ Backend/routes/evaluationRouter.js
@@ -14,7 +14,7 @@ router.use(requireAuthUser,controledAcces('Candidat'), authLogMiddleware("Evalua
 
 router.post('/generate-questions', evaluationController.generateQuestions);
 router.post('/generate-technique-questions', evaluationController.generateTechniqueQuestions);
-router.post('/job/:id/generate-technique-questions', evaluationController.generateTechniqueQuestionsForJob);
+router.post('/job/:id/generate-technique-questions',requireAuthUser, evaluationController.generateTechniqueQuestionsForJob);
 router.post('/generate-soft-skill-questions', evaluationController.generateSoftSkillQuestions);
 router.post('/analyze-profile-answers', evaluationController.analyzeProfileAnswers);
 router.post('/analyze-job-test-results', evaluationController.analyzeJobTestResults);
