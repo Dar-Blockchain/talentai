e89faa904ed4caf9e980cf1c2838dc4eca463256
jassemtalbi <jassemtalbi2@gmail.com>
2025-07-15 12:20:27 +0100

    Update HackathonRegistration to use dynamic API base URL for project fetching

--- src/pages/hackathon-registration.tsx
+++ src/pages/hackathon-registration.tsx
@@ -80,7 +80,9 @@ const HackathonRegistration = () => {
       setCheckingProject(true);
       try {
         const token = localStorage.getItem('api_token');
-        const res = await fetch('http://localhost:5000/project/getMyProjects', {
+        const url = `${process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5000/'}project/getMyProjects'`;
+
+        const res = await fetch(url, {
           headers: token ? { Authorization: `Bearer ${token}` } : {},
         });
         if (res.ok) {
