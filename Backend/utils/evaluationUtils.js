fa6d96de2e75f03794652990cb598b01823f4016
monabeldi <mona_beldi@outlook.com>
2025-07-15 15:38:41 +0200

    save hr interviewDetails with  recommendations

--- Backend/utils/evaluationUtils.js
+++ Backend/utils/evaluationUtils.js
@@ -271,8 +271,10 @@ async function saveInterviewDetails(
   profile,
   overallScore,
   skillAnalysis,
-  formData
+  formData,
+  recommendations
 ) {
+  console.log("check : recommendations", recommendations);
   const details = skillAnalysis.map((skill) => ({
     name: skill.skillName,
     type: SKILL_TYPES.SOFT,
@@ -293,6 +295,7 @@ async function saveInterviewDetails(
     overallScore: overallScore,
     interviewContext: formData ? formData : null,
     skillDetails: details,
+    recommendations: recommendations,
   });
 
   await interviewDetails.save();
