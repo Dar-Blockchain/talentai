0419a421673259dc91fd77d0ce7117db9e33f358
AzizBenIsmail <aziz.270700@gmail.com>
2025-10-27 16:54:58 +0100

    prend en charge les nouveaux modification

--- Backend/utils/evaluationUtils.js
+++ Backend/utils/evaluationUtils.js
@@ -401,6 +401,8 @@ async function saveInterviewDetailsForAddSkill(
       answer: qa.answer || "unanswered",
       status: qa.status,
       exampleCorrectAnswer: qa.exampleCorrectAnswer || null,
+      partialCorrectPercentage: qa.partialCorrectPercentage || null,
+      partialCorrectReason: qa.partialCorrectReason || null,
     })),
   }));
 
