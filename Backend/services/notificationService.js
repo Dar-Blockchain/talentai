3e868cda1d5d6c42ee3cd97f99bae83c9dd49a31
AzizBenIsmail <aziz.270700@gmail.com>
2025-11-25 12:15:50 +0100

    Delete notificationService.js

--- Backend/services/notificationService.js
+++
@@ -1,5 +0,0 @@
-const Notification = require('../models/notificationModel');
-
-exports.createNotification = async ({ content, type, url, recipient }) => {
-  return await Notification.create({ content, type, url, recipient });
-};
