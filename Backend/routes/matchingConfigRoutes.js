593b12cc707c5efbee2cd9ea53b4015b4addbf62
AzizBenIsmail <aziz.270700@gmail.com>
2025-11-29 04:03:36 +0100

    Sécurise

--- Backend/routes/matchingConfigRoutes.js
+++ Backend/routes/matchingConfigRoutes.js
@@ -2,6 +2,15 @@ const express = require('express');
 const router = express.Router();
 const { getConfig, updateConfig } = require('../controllers/MatchingController/MatchingConfigController');
 
+// Import des middlewares
+const { requireAuthUser } = require('../middleware/authMiddleware');
+const { controledAcces } = require('../middleware/controledAcces'); 
+const authLogMiddleware = require("../middleware/SystemeLogs/LogMiddleware")
+
+
+// Toutes les routes ci-dessous nécessitent un admin authentifié
+router.use(requireAuthUser, controledAcces('Admin'), authLogMiddleware("MatchingConfig"));
+
 router.get('/', getConfig);
 router.put('/', updateConfig);
 
