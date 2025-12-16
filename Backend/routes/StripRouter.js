67ac7b81dc8378abbd3fcbac6a7249cecabe4760
AzizBenIsmail <aziz.270700@gmail.com>
2025-12-16 12:28:30 +0100

    processStripeSession

--- Backend/routes/StripRouter.js
+++ Backend/routes/StripRouter.js
@@ -6,19 +6,16 @@ const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
 // ✅ Route pour créer une session de paiement Stripe
 const stripeController = require('../controllers/PaymentContollers/stripeController');
-const paymentController = require('../controllers/paymentController');
 
 router.post('/create-checkout-session', stripeController.createCheckoutSession);
 
-router.post('/complete-payment', paymentController.processStripeSession);
 
-
-// ✅ Webhook Stripe
-router.post(
-  '/webhook',
-  bodyParser.raw({ type: 'application/json' }),
-  // Delegate webhook handling to controller (expects raw body)
-  paymentController.handleStripeWebhook
-);
+// // ✅ Webhook Stripe
+// router.post(
+//   '/webhook',
+//   bodyParser.raw({ type: 'application/json' }),
+//   // Delegate webhook handling to controller (expects raw body)
+//   paymentController.handleStripeWebhook
+// );
 
 module.exports = router;
