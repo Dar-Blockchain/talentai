9a41f5b33421d3ef624b3ffb95a39fbc58929d09
AzizBenIsmail <aziz.270700@gmail.com>
2026-01-08 15:23:30 +0100

    Update authenticationService.js

--- Backend/services/AuthenticationServices/authenticationService.js
+++ Backend/services/AuthenticationServices/authenticationService.js
@@ -171,7 +171,7 @@ exports.verifyUserOTP = async (email, otp, location = null) => {
     // Fetch profile and companyMembership in parallel if exist
     const [profile, companyMembership] = await Promise.all([
       user.profile ? Profile.findById(user.profile) : null,
-      user.companyMembership ? require('../../models/CompanyMembershipModel').findById(user.companyMembership).lean().select('_id role company') : null
+      user.companyMembership ? require('../../models/CompanyMembershipModel').findById(user.companyMembership).populate('company').select('_id role company') : null
     ]);
 
     console.log('✅ OTP verified successfully for:', email);
