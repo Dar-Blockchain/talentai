dbf88421b1fbc0b3226ea418c2931897695a6857
AzizBenIsmail <aziz.270700@gmail.com>
2026-01-30 10:55:53 +0100

    Update routes.js

--- Backend/config/routes.js
+++ Backend/config/routes.js
@@ -57,8 +57,8 @@ function registerRoutes(app) {
   // Company Management
   app.use('/CompanyInvitation', CompanyInvitationRouters); // Company Invitation Management -> company-invitations
   app.use('/CompanyMembership', CompanyMembershipRoutes); // Company Membership Management -> company-memberships
-  //app.use('/company-invitations', CompanyInvitationRouters); // Company Invitation Management
-  //app.use('/company-memberships', CompanyMembershipRoutes); // Company Membership Management
+  //app.use('/company-invitations', CompanyInvitationRouters); //✅ Company Invitation Management
+  //app.use('/company-memberships', CompanyMembershipRoutes); //✅ Company Membership Management
 
   // Evaluation & Interview
   app.use('/skill-interview-assessments', SkillInterviewAssessmentRoutes); //✅ Skill Interview Assessments -> skill-interview-assessments
