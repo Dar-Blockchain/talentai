6f382caf4186bba5080783d44c5dd6bcf428260f
AzizBenIsmail <aziz.270700@gmail.com>
2026-03-10 10:45:35 +0100

    fix path for resume

--- Backend/middleware/fileResume-upload.middleware.js
+++ Backend/middleware/fileResume-upload.middleware.js
@@ -3,10 +3,10 @@ const path = require('path')
 const fs = require('fs')
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public/images/Users')
+    cb(null, 'public/resume')
   },
   filename: function (req, file, cb) {
-    const uploadPath = 'public/images/Users';
+    const uploadPath = 'public/resume';
     const originalName = file.originalname;
     console.log(file.originalname)
     const fileExtension = path.extname(originalName);
