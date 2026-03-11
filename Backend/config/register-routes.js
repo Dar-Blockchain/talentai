35024c0a9ec86584383dd8ef15cda638c50ceffc
AzizBenIsmail <aziz.270700@gmail.com>
2026-03-11 15:48:13 +0100

    Update register-routes.js

--- Backend/config/register-routes.js
+++ Backend/config/register-routes.js
@@ -131,8 +131,8 @@ function registerRoutes(app) {
   });
 
   // Register internal campaign routes
-  // app.use('/internal-campaigns', internalCampaignRoutes);
-  // app.use('/campaign-participants', campaignParticipantRoutes);
+  app.use('/internal-campaigns', internalCampaignRoutes);
+  app.use('/campaign-participants', campaignParticipantRoutes);
   // app.use('/departments', departmentRoutes);
   app.use('/contact', contactRouter);
   app.use('/interview-applicants', interviewApplicantRouter);
