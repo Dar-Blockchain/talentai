64d0b74fe8d8a1116fbe33683e0b21713e3deb27
AzizBenIsmail <aziz.270700@gmail.com>
2026-04-14 12:50:37 +0100

    Update post.service.js

--- Backend/services/PosteServices/post.service.js
+++ Backend/services/PosteServices/post.service.js
@@ -408,7 +408,7 @@ module.exports.getAllPostsWithSearch = async (filters = {}, page = 1, limit = 6)
 // Get a post by its ID
 module.exports.getPostById = async (postId) => {
   try {
-    const post = await Post.findById(postId).populate();
+    const post = await Post.findById(postId).populate("PostSteps");
     if (!post) {
       throw new Error("Post not found");
     }
