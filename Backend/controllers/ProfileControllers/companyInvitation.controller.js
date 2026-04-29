4f65626425d39adee3c6d7fa8ac65dc9dd043ebf
AzizBenIsmail <aziz.270700@gmail.com>
2026-04-29 12:21:34 +0100

    fix

--- Backend/controllers/ProfileControllers/companyInvitation.controller.js
+++ Backend/controllers/ProfileControllers/companyInvitation.controller.js
@@ -1,4 +1,4 @@
-const CompanyInvitationService = require("../../services/ProfileService/company.Invitation.service");
+const CompanyInvitationService = require("../../services/ProfileService/companyInvitation.service");
 const authService = require("../../services/authentication.service");
 const { generateToken } = require("../../utils/generate-token");
 
--- Backend/controllers/ProfileControllers/companyMembership.controller.js
+++ Backend/controllers/ProfileControllers/companyMembership.controller.js
@@ -1,5 +1,5 @@
-const CompanyMembershipService = require("../../services/ProfileService/company.Membership.service");
-const CompanyInvitationService = require("../../services/ProfileService/company.Invitation.service");
+const CompanyMembershipService = require("../../services/ProfileService/companyMembership.service");
+const CompanyInvitationService = require("../../services/ProfileService/companyInvitation.service");
 
 const flattenMembership = (membership) => {
   const m = membership?.toObject ? membership.toObject() : membership;
