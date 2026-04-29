e4c18eda4e9c3dc585e35fe05fc14437e45bdff0
AzizBenIsmail <aziz.270700@gmail.com>
2026-04-29 12:19:55 +0100

    fix SkillInterviewAssessment

--- Backend/controllers/InterviewControllers/skillsInterviewAssessment.controller.js
+++ Backend/controllers/InterviewControllers/skillsInterviewAssessment.controller.js
@@ -1,4 +1,4 @@
-const SkillInterviewAssessmentService = require("../../services/InterviewServices/SkillInterviewAssessment.service");
+const SkillInterviewAssessmentService = require("../../services/InterviewServices/skill.InterviewAssessment.service");
 const Profile = require("../../models/Profile.model");
 
 // ========== POST - Create a new assessment ==========
