c879697eee0feadac67dba1ca49ee5d46c749dcf
AzizBenIsmail <aziz.270700@gmail.com>
2026-05-07 16:41:19 +0100

    Revert "Reapply "unsed api""
    This reverts commit b86722af2222e2c555fd4b09ed0645d919bfdc78.

--- Backend/controllers/planLimits.controller.js
+++ Backend/controllers/planLimits.controller.js
@@ -11,6 +11,33 @@ const handleError = (res, error, defaultStatus = 500) => {
 };
 
 /**
+ * POST /planLimits - Create a new plan
+ */
+module.exports.createPlan = async (req, res) => {
+  try {
+    const {
+      name,
+      postsLimit,
+      monthlyInterviewLimit,
+      description,
+      isActive,
+    } = req.body;
+
+    const result = await planLimitsService.createPlan({
+      name,
+      postsLimit,
+      monthlyInterviewLimit,
+      description,
+      isActive,
+    });
+
+    res.status(201).json(result);
+  } catch (error) {
+    handleError(res, error, 400);
+  }
+};
+
+/**
  * GET /planLimits - Get all plans with optional filters
  */
 module.exports.getAllPlans = async (req, res) => {
@@ -27,6 +54,21 @@ module.exports.getAllPlans = async (req, res) => {
     res.status(200).json(result);
   } catch (error) {
     handleError(res, error);
+  }
+};
+
+/**
+ * GET /planLimits/:id - Get plan by ID
+ */
+module.exports.getPlanById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const result = await planLimitsService.getPlanById(id);
+
+    res.status(200).json(result);
+  } catch (error) {
+    handleError(res, error, 404);
   }
 };
 
