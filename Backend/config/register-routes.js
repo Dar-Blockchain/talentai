5e51f1d2dc0788527685c46c62a6f2ea7db00cc2
AzizBenIsmail <aziz.270700@gmail.com>
2026-05-07 15:39:40 +0100

    Update register-routes.js

--- Backend/config/register-routes.js
+++ Backend/config/register-routes.js
@@ -74,10 +74,10 @@ function registerRoutes(app) {
   app.use("/job-applications", jobApplicationRouter); //✅ Job Applications -> job-applications
 
   // Utility & Management
- // app.use("/todo", todoRouter); //✅ To-Do Management -> todos
+// app.use("/todo", todoRouter); //✅ To-Do Management -> todos
   app.use("/feedback", feedbackRouter); //✅ Feedback Management -> feedbacks
   app.use("/logs", logRoutes); //✅ System Logs Management -> logs
-  app.use("/task", taskRouter); //✅ Task Management -> tasks
+//app.use("/task", taskRouter); //✅ Task Management -> tasks
   app.use('/admin/backups', backupRouter); //✅ Database Backup Management -> admin/backups
 
   // Billing
