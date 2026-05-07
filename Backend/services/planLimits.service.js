d433d39862bdc75730ed97aa6edc142bb5550b9a
AzizBenIsmail <aziz.270700@gmail.com>
2026-05-07 12:06:59 +0100

    unsed api

--- Backend/services/planLimits.service.js
+++ Backend/services/planLimits.service.js
@@ -2,38 +2,6 @@ const PlanLimits = require("../models/PlanLimits.model");
 const Profile = require("../models/Profile.model");
 
 /**
- * Create a new plan
- */
-module.exports.createPlan = async (planData) => {
-  try {
-    if (!planData.name) {
-      const err = new Error("Plan name is required");
-      err.status = 400;
-      throw err;
-    }
-
-    const existingPlan = await PlanLimits.findOne({ name: planData.name });
-    if (existingPlan) {
-      const err = new Error("Plan with this name already exists");
-      err.status = 409;
-      throw err;
-    }
-
-    const plan = new PlanLimits(planData);
-    await plan.save();
-
-    return {
-      success: true,
-      message: "Plan created successfully",
-      data: plan,
-    };
-  } catch (error) {
-    console.error("Error creating plan:", error);
-    throw error;
-  }
-};
-
-/**
  * Get all plans
  */
 module.exports.getAllPlans = async (filters = {}) => {
@@ -53,35 +21,6 @@ module.exports.getAllPlans = async (filters = {}) => {
     };
   } catch (error) {
     console.error("Error fetching plans:", error);
-    throw error;
-  }
-};
-
-/**
- * Get plan by ID
- */
-module.exports.getPlanById = async (planId) => {
-  try {
-    if (!planId) {
-      const err = new Error("Plan ID is required");
-      err.status = 400;
-      throw err;
-    }
-
-    const plan = await PlanLimits.findById(planId);
-
-    if (!plan) {
-      const err = new Error("Plan not found");
-      err.status = 404;
-      throw err;
-    }
-
-    return {
-      success: true,
-      data: plan,
-    };
-  } catch (error) {
-    console.error("Error fetching plan:", error);
     throw error;
   }
 };
