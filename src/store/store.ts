9f4e22e0cf5772b1f5ebb402a604715cf36cc367
jassemtalbi <jassemtalbi2@gmail.com>
2026-05-25 12:31:52 +0100

    refactor: consolidate post details types and utilities, and update JobPostCard and PostDetailsPage components

--- src/store/store.ts
+++ src/store/store.ts
@@ -31,6 +31,7 @@ import jobApplicationReducer from './slices/jobApplicationSlice';
 import feedbackReducer from './slices/feedbackSlice';
 import paymentReducer from './slices/paymentSlice';
 import kpiReducer from './slices/kpiSlice';
+import postDetailsReducer from '@/modules/posts/details/store/postSlice';
 import { socketMiddleware } from './middleware/socketMiddleware';
 
 const rootReducer = combineReducers({
@@ -55,6 +56,7 @@ const rootReducer = combineReducers({
   kpi: kpiReducer,
   feedback: feedbackReducer,
   payment: paymentReducer,
+  postDetails: postDetailsReducer,
 });
 
 const persistConfig = {
