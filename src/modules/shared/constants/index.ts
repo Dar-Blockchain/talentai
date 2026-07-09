000b71a5df3775dc7b7cd457efb3df75ef24cb19
Ali Al-Shanti <alialshanti998@gmail.com>
2026-07-09 17:20:07 +0300

    fix: update Calendly URL for first contact and standardize formatting in constants

--- src/modules/shared/constants/index.ts
+++ src/modules/shared/constants/index.ts
@@ -1,15 +1,15 @@
-export const SITE_URL      = "https://talentai.bid";
-export const APP_URL       = "https://app.talentai.bid";
+export const SITE_URL = "https://talentai.bid";
+export const APP_URL = "https://app.talentai.bid";
 
 export const CONTACT_EMAIL = "contact@talentai.bid";
 export const SUPPORT_EMAIL = "support@talentai.bid";
 
-export const CALENDLY_URL  = "https://calendly.com/talent__ai/30min";
+export const CALENDLY_URL = "https://calendly.com/talent__ai/first-contact";
 
-export const LINKEDIN_URL  = "https://www.linkedin.com/company/talentai-bid/";
-export const TWITTER_URL   = "https://x.com/talentai_bid";
-export const YOUTUBE_URL   = "https://www.youtube.com/@TalentAi-w3p";
-export const GITHUB_URL    = "https://github.com/TalentAI-bid/";
+export const LINKEDIN_URL = "https://www.linkedin.com/company/talentai-bid/";
+export const TWITTER_URL = "https://x.com/talentai_bid";
+export const YOUTUBE_URL = "https://www.youtube.com/@TalentAi-w3p";
+export const GITHUB_URL = "https://github.com/TalentAI-bid/";
 
-export const OG_IMAGE      = `${SITE_URL}/images/home/og-cover.png`;
-export const LOGO_URL      = `${SITE_URL}/logo.svg`;
+export const OG_IMAGE = `${SITE_URL}/images/home/og-cover.png`;
+export const LOGO_URL = `${SITE_URL}/logo.svg`;
