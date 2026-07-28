a26410cf41a731aa30983f155eba041c57acfaf2
Ali Al-Shanti <alialshanti998@gmail.com>
2026-07-28 12:26:20 +0300

    fix(Header): replace InterviewHeader with HomeHeader in CampaignSessionPage

--- src/modules/shared/layouts/home/HomeHeader.tsx
+++ src/modules/shared/layouts/home/HomeHeader.tsx
@@ -35,7 +35,7 @@ const Header = () => {
   const [unreadMessageCount, setUnreadMessageCount] = useState(0);
 
   const landingLikePaths = useMemo(
-    () => ["/", "/terms", "/privacy", "/blog", "/blog/[slug]"],
+    () => ["/", "/terms", "/privacy", "/blog", "/blog/[slug]", "/campaigns/sessions/[campaignId]"],
     []
   );
 
